79bc122d94b25ff05ea77cb02fecd0884abcc8d1
Joshreigns <60883219+Joshreigns@users.noreply.github.com>
2026-07-31 16:59:11 +0800

    Update getspecs.ps1

--- getspecs.ps1
+++ getspecs.ps1
@@ -417,114 +417,7 @@ $Inventory.Windows = [ordered]@{
 
 }
 
-# -------------------------------
-# Microsoft Office Information
-# -------------------------------
 
-$Inventory.Office = [ordered]@{
-
-    Installed = "No"
-
-    Product = "Not Found"
-
-    Version = "Not Found"
-
-    Architecture = "Not Found"
-
-    InstallType = "Not Found"
-
-}
-
-
-
-$OfficeRegistryPaths = @(
-
-"HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration",
-
-"HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\ClickToRun\Configuration",
-
-"HKLM:\SOFTWARE\Microsoft\Office\16.0\Common\InstallRoot",
-
-"HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\16.0\Common\InstallRoot"
-
-)
-
-
-
-foreach ($Path in $OfficeRegistryPaths)
-{
-
-    if(Test-Path $Path)
-    {
-
-        try
-        {
-
-            $Office =
-            Get-ItemProperty $Path
-
-
-
-            $Inventory.Office.Installed =
-            "Yes"
-
-
-
-            if($Office.ProductReleaseIds)
-            {
-
-                $Inventory.Office.Product =
-                $Office.ProductReleaseIds
-
-            }
-
-
-
-            if($Office.Version)
-            {
-
-                $Inventory.Office.Version =
-                $Office.Version
-
-            }
-
-
-
-            if($Office.Platform)
-            {
-
-                $Inventory.Office.Architecture =
-                $Office.Platform
-
-            }
-
-
-
-            if($Path -like "*ClickToRun*")
-            {
-
-                $Inventory.Office.InstallType =
-                "Click-to-Run"
-
-            }
-            else
-            {
-
-                $Inventory.Office.InstallType =
-                "MSI"
-
-            }
-
-
-        }
-        catch
-        {
-
-        }
-
-    }
-
-}
 
 
 # -------------------------------
@@ -1633,33 +1526,17 @@ Operating System
 </div>
 
 <div class="value">
-$($Inventory.Windows.Caption)
-<br>
-$($Inventory.Windows.Architecture)
+$($Inventory.Windows.Caption) $($Inventory.Windows.Architecture)
 </div>
-
 </div>
-
-
-
-<div class="item">
-
-<div class="label">
-Microsoft Office
 </div>
-
-<div class="value">
-$($Inventory.Office.Product)
-<br>
-$($Inventory.Office.Version)
-<br>
-$($Inventory.Office.Architecture)
 </div>
-
 </div>
-
-
 "@
+
+
+
+
 
 # ============================================================
 # CPU SECTION
